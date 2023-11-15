b152356ac2b8a2b62815d1208a0311e468ae5c88
Sebastian Garcia -usumy <118477084+usumy@users.noreply.github.com>
2023-11-15 13:52:29 -0600

    Add files via upload

---
+++ storage/framework/views/b379193233828ac6b6edbfbb86beb32e2fa6cc61.php
@@ -0,0 +1,22 @@
+<script ></script>
+<div class="container mt-5">
+    <h2>Fallas Registradas</h2>
+    <table class="table" src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Descripción</th>
+                <th scope="col">Prioridad</th>
+                <th scope="col">Departamento</th>
+                <th scope="col">Nombre del Empleado</th>
+                <th scope="col">Resuelto</th>
+            </tr>
+        </thead>
+        <tbody>
+           
+        </tbody>
+    </table>
+</div>
+
+
+<?php /**PATH C:\laragon\www\falla\resources\views/fallas.blade.php ENDPATH**/ ?>
