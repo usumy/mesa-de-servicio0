461e0668969c6c5aad31d5a0a87111a719747afd
Sebastian Garcia -usumy <118477084+usumy@users.noreply.github.com>
2023-11-24 15:03:47 -0600

    Add files via upload

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,6 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'rol_id' => ['required','confirmed'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
@@ -68,7 +67,6 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'rol_id' => $data['rol_id'],
             'password' => Hash::make($data['password']),
         ]);
     }
